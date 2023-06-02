ed26eda8e6357dce79d90ab616a2a8abac69f218
ythiero <35019@ma-web.nl>
2023-06-02 10:29:17 +0200

    aa yes coler

---
+++ coler/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace coler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Colors myMainThemeColor = Colors.White;
+            if (myMainThemeColor== Colors.White)
+            {
+                Console.WriteLine("yes boy");
+            }
+        }
+    }
+}
