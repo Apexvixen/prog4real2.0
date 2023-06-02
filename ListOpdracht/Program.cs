2f288dd9e1b4a3e169c3fae187a6aad56e706aac
ythiero <35019@ma-web.nl>
2023-06-02 10:19:37 +0200

    list stuff

---
+++ ListOpdracht/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ListOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> bird = new List<string>() {"uil", "kraai", "hond","papegaai"};
+            List<string> bird2 = new List<string>() { "meeuw", "duil" };
+            bird.AddRange(bird2);
+            bird.Remove("hond");
+            foreach (string s in bird)
+            {
+                Console.WriteLine(s);
+            }
+            
+
+
+        }
+    }
+}
